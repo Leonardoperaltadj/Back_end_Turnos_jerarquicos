723e47f8dbdeab6ed8ec0d488ed161fdb24184bb
LeonardoPeralta <peraltauriel4@gmail.com>
2023-07-26 15:29:22 -0600

    Se agrega relacion con la tabla cuentas

--- src/models/entities/catAccountTypes.py
+++ src/models/entities/catAccountTypes.py
@@ -1,11 +1,11 @@
 from database.db import db
 
-class catAccountTypes(db.Model):
+class CatAccountTypes(db.Model):
     __tablename__ = 'cat_tipo_cuenta'
 
     id_cat_tipo_cuenta = db.Column(db.SmallInteger, primary_key=True)
     cuenta = db.Column(db.String(20), nullable=False)
-    #usuarios = db.relationship('User', backref='cat_roles', lazy = True )
+    accounts = db.relationship('Accounts', backref='cat_tipo_cuenta', lazy = True )
 
     def __init__(self, id_cat_tipo_cuenta, cuenta=None):
         self.id_cat_tipo_cuenta=id_cat_tipo_cuenta
