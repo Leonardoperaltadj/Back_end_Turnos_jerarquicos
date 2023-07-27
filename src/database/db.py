841db535675aee63a8aa193036e670af18ada789
LeonardoPeralta <peraltauriel4@gmail.com>
2023-07-26 18:19:56 -0600

    Se restablece el cache

--- src/database/db.py
+++ src/database/db.py
@@ -14,7 +14,7 @@ user=os.environ['PGSQL_USER']
 password=os.environ['PGSQL_PASSWORD']
 database=os.environ['PGSQL_DATABASE']
 
-DATEBASE_CONNECTION_URI= f'postgresql://{user}:{password}@{host}:5432/{database}'
+DATEBASE_CONNECTION_URI= f'postgresql://{user}:{password}@{host}:{port}/{database}'
 
 def get_connection():
     try:
